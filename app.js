eec3fc4e0857058719e5b7ccde05cc3ddf4f67b8
mogulla3 <hs.iarn3@gmail.com>
2018-09-22 15:15:55 +0900

    Try cookie-session

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieSession = require('cookie-session')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
-const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(cookieSession({
+  name: 'hello-express-session',
+  keys: ['secret1', 'secret2'],
+  maxAge: 60 * 60 * 24 * 1000,
+  httpOnly: true
+}))
 app.use(bodyParser.json()); // for application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for application/x-www-form-urlencoded
 app.use('/', indexRouter)
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,34 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res) {
-  res.send('GET /')
+  if (req.session.user_id) {
+    res.send(`hi! user_id = ${req.session.user_id}`)
+  } else {
+    res.send('hi! anonymous. <a href="/login">login</a>')
+  }
+})
+
+router.get('/login', function(req, res) {
+  if (req.session.user_id) {
+    res.send(`you are already logged in`)
+  } else {
+    req.session.user_id = Math.floor(Math.random(1, 100) * 100)
+    res.send('login success!')
+  }
+})
+
+router.get('/logout', function(req, res) {
+  req.session = null
+  res.send('logout success')
+})
+
+router.get('/views', function(req, res) {
+  if (req.session.user_id) {
+    req.session.views = (req.session.views || 0) + 1
+    res.send(`GET /: user_id=${req.session.user_id} views=${req.session.views}`)
+  } else {
+    res.redirect('/')
+  }
 })
 
 module.exports = router
