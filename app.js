3e8fd0a2ed980f4a11cb178e7f0f6f1e47d20a6e
mogulla3 <hs.iarn3@gmail.com>
2018-09-22 13:15:53 +0900

    Change some files

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express()
 
-var app = express();
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(3000, () => console.log('listen..'))
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function(req, res) {
+  res.send('GET /users')
+})
 
-module.exports = router;
+router.get('/:userId', function(req, res) {
+  console.log(req.params)
+  console.log(req.query)
+  res.send(`hi! userId=${req.params.userId}`)
+})
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+module.exports = router
